589698e9d014fe72fcea79e07b766cd058dad7b3
MoonSakura <lry990416@gmail.com>
2025-07-14 12:08:24 +0800

    refactor: restructure project files into include/ and src/ directories

--- Moon/src/Engine/Engine.cpp
+++
@@ -1,7 +0,0 @@
-#include <windows.h>
-
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
-{
-    MessageBoxA(nullptr, "Hello, DirectX 12 world!", "My First App", MB_OK | MB_ICONINFORMATION);
-    return 0;
-}
---
+++ Moon/src/MoonEngine.cpp
@@ -0,0 +1,9 @@
+#include <windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+                   LPSTR lpCmdLine, int nShowCmd)
+{
+    MessageBoxA(nullptr, "Hello, DirectX 12 world!", "My First App",
+                MB_OK | MB_ICONINFORMATION);
+    return 0;
+}
